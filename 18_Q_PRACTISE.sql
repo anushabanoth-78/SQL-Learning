22136f97b14b8ffa9bcd3c4bb648ae993434b0e9
Anusha Banoth <banothanusha900@gmail.com>
2026-09-23 21:14:01 +0530

    Create employee table and insert sample data
    Created a new database and employee table with sample data and various queries.

--- 18_Q_PRACTISE.sql
+++ 18_Q_PRACTISE.sql
@@ -358,6 +358,145 @@ select * from Employee;
 DROP TABLE  Employee;
 
 
+-- Create the employee table
+CREATE DATABASE pra;
+USE pra;
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    department VARCHAR(50),
+    salary DECIMAL(10, 2),
+    city VARCHAR(50)
+);
+
+-- Insert the data
+INSERT INTO employees (employee_id, name, department, salary, city) VALUES
+(1, 'Anu', 'IT', 50000.00, 'Hyderabad'),
+(2, 'Ravi', 'HR', 60000.00, 'Bangalore'),
+(3, 'Priya', 'IT', 75000.00, 'Hyderabad'),
+(4, 'Rahul', 'Finance', 80000.00, 'Mumbai'),
+(5, 'Neha', 'IT', 65000.00, 'Hyderabad'),
+(6, 'Kiran', 'HR', 55000.00, 'Chennai');
+SELECT *FROM employees;
+
+SELECT name, salary
+FROM employees
+WHERE salary >60000.00;
+
+SELECT name,salary,city
+FROM employees
+WHERE salary>60000.00
+AND department ='IT';
+
+SELECT *FROM employees
+WHERE city = 'Hyderabad';
+
+SELECT name,salary
+FROM employees
+WHERE salary BETWEEN 55000 AND 75000;
+
+SELECT name AS eitherithr
+FROM employees
+WHERE department ='IT' OR 'HR';
+
+
+SELECT name, salary
+FROM employees
+ORDER BY salary DESC;
+
+
+SELECT name, salary
+FROM employees
+ORDER BY salary DESC
+LIMIT 3;
+
+SELECT COUNT(*)
+FROM employees;
+
+SELECT AVG(salary)
+FROM employees;
+
+SELECT *FROM employees
+ORDER BY salary DESC
+LIMIT 1;
+
+SELECT *FROM employees
+ORDER BY salary DESC
+LIMIT 1 OFFSET 5;
+
+SELECT *FROM employees
+ORDER BY salary ASC
+LIMIT 1 ;
+
+SELECT MIN(salary)
+FROM employees;
+
+SELECT *FROM employees;
+
+SELECT department,AVG(salary) AS averagesalary
+FROM employees
+GROUP BY department;
+
+SELECT department,AVG(salary) AS averagesalary
+FROM employees
+GROUP BY department
+HAVING AVG(salary) > 60000;
+
+SELECT *FROM employees;
+
+SELECT city, count(*) AS totalcites
+FROM employees
+GROUP BY city;
+
+SELECT department, AVG(salary) AS highestsalary 
+FROM employees
+GROUP BY department
+ORDER BY avg(salary) DESC
+LIMIT 1;
+
+SELECT *FROM employees
+ORDER BY salary DESC
+LIMIT 1 OFFSET 1;
+
+SELECT name
+FROM employees
+WHERE name 
+LIKE'A%';
+
+SELECT name
+FROM employees
+WHERE name 
+LIKE'%a';
+
+SELECT name, department,salary
+FROM employees
+WHERE salary <>60000;
+
+SELECT *FROM employees AS highestsalary 
+ORDER BY salary DESC
+LIMIT 1;
+
+SELECT *FROM employees AS highestsalary 
+ORDER BY salary DESC
+LIMIT 1 OFFSET 5;
+
+SELECT AVG(salary) AS highestsalary
+FROM employees
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
